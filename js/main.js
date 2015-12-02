e617bd7576cca1df607b7793fc3c5903eecd9ccf
Caleb Scholze <caleb.scholze@gmail.com>
2015-12-02 15:03:59 -0600

    Clean house

--- js/main.js
+++
@@ -1,9 +0,0 @@
-define(["eventHandlers","songLibrary","populatePlaylist","populateFilter"], function(eventHandlers,songLibrary,populatePlaylist,populateFilter) {
-	$.when( songLibrary.getSnapshot() ).then(function(firebaseSnapshot) {
-		populatePlaylist(firebaseSnapshot);
-		// populateFilter(firebaseSnapshot);
-	});
-
-
-
-}); // END MAIN MODULE
--- js/populatePlaylist.js
+++
@@ -1,8 +0,0 @@
-define(["jquery"], function($) {
-	return function(snapshot) {
-		// Pass snapshot object to HBS plugin to populate DOM
-		require( ['hbs!../templates/playlist'], function(playlistTemplate) {
-			$(".playlist").html(playlistTemplate(snapshot));
-		});
-	};
-});
